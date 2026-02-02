df77b4140a8a5b558cf498aa609b61b4c836a37d
Babli Sharma <bablithv101@gmail.com.com>
2026-02-02 17:46:33 +0530

    renderMode update

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,51 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  /* -------- STATIC ROUTES (PRERENDER) -------- */
+  {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'services',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'in-box-desiccants',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'in-box-desiccants/incubic',
+    renderMode: RenderMode.Prerender,
+  },
+
+  /* -------- DYNAMIC ROUTES (CLIENT ONLY) -------- */
+  {
+    path: 'in-box-desiccants/incubic/:category',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'in-box-desiccants/incubic/:category/:product',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'product/:id',
+    renderMode: RenderMode.Client,
+  },
+
+  /* -------- FALLBACK -------- */
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Prerender,
   }
+
 ];
